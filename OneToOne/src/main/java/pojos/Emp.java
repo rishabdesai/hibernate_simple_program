d8a63ed87293a5f238d17a08a28e4dc80961b00f
Rishabh Desai <desai.rishab@gmail.com>
2021-04-27 08:36:51 +0530

    .

--- OneToOne/src/main/java/pojos/Emp.java
+++ OneToOne/src/main/java/pojos/Emp.java
@@ -4,10 +4,7 @@ import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
